b2ffa3da4f4e01e5ca54a53f2d46e987aa0b66a1
uzay00 <uzay.cetin@isikun.edu.tr>
2017-04-16 00:31:47 +0300

    Update 01.piSimulation_withPlots.R

--- 01.piSimulation_withPlots.R
+++ 01.piSimulation_withPlots.R
@@ -1,20 +1,12 @@
 #dataistanbul talk - April 5th, 2017
 #HS Olmez (c) 2017
-# Türkçe açıklamalar: Uzay Çetin
-
-########################################################
-# Amaç, istatistik kullanarak
-#     pi'nin değerini hesaplamak
-########################################################
-# N adet nokta üretir
+#
+# For points in only a quarter of the circle
 piR <- function(N) {
-  x <- runif(N,0,1) # Her noktanın x bileşeni
-  y <- runif(N,0,1) # Her noktanın y bileşeni
-  
-  # orijine olan mesafe
-  d <- sqrt(x^2 + y^2) 
-  # mesafe, r=1'den küçükse, nokta pi/4'lük alan içinde kalır
-  return(4*sum(d<1.0)/N) # pi/4'lük alan içinde kalan nokta sayısı
+  x <- runif(N,0,1)
+  y <- runif(N,0,1)
+  d <- sqrt(x^2 + y^2)
+  return(4*sum(d<1.0)/N)
 }
 set.seed(7) #for reproducibility
 cat(piR(1000),piR(10000),piR(100000),piR(1000000))
